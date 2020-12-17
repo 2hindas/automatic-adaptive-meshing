a79f6f3dfaa5181a561425d0769efc5f0fd1d4ce
Tuhin Das <t.das@student.tudelft.nl>
2020-12-17 10:31:07 +0100

    Added Lars' code in this branch and refactored some files.

--- src/LapentaEstimator.py
+++ src/LapentaEstimator.py
@@ -5,6 +5,10 @@ import numpy as np
 import SimPEG.electromagnetics.frequency_domain as fdem
 from scipy.interpolate import Rbf, LinearNDInterpolator, NearestNDInterpolator
 import numdifftools as nd
+from src.Meshing import refine_at_locations, create_octree_mesh
+from SimPEG.utils import surface2ind_topo
+from src.Setup import search_area_receivers, search_area_object
+from SimPEG import maps
 
 try:
     from pymatsolver import Pardiso as Solver
@@ -12,32 +16,41 @@ except ImportError:
     from SimPEG import SolverLU as Solver
 
 
+# Input of this function are the x-,y- and z-faces and the curl values of the electric field on the faces
+# Or x-,y- and z-edges and the values of the electric field
+# Radial basis function interpolation
 def interpolate_rbf(x, y, z, x_val, y_val, z_val):
-    x_curl_interpolated = Rbf(x[:, 0], x[:, 1], x[:, 2], x_val)
-    y_curl_interpolated = Rbf(y[:, 0], y[:, 1], y[:, 2], y_val)
-    z_curl_interpolated = Rbf(z[:, 0], z[:, 1], z[:, 2], z_val)
+    x_interpolated = Rbf(x[:, 0], x[:, 1], x[:, 2], x_val)
+    y_interpolated = Rbf(y[:, 0], y[:, 1], y[:, 2], y_val)
+    z_interpolated = Rbf(z[:, 0], z[:, 1], z[:, 2], z_val)
 
-    return x_curl_interpolated, y_curl_interpolated, z_curl_interpolated
+    return x_interpolated, y_interpolated, z_interpolated
 
 
+# Input of this function are the x-,y- and z-faces and the curl values of the electric field on the faces
+# Or x-,y- and z-edges and the values of the electric field
+# Nearest neighbour interpolation
 def interpolate_nearest(x, y, z, x_val, y_val, z_val):
-    x_curl_interpolated = NearestNDInterpolator(x, x_val)
-    y_curl_interpolated = NearestNDInterpolator(y, y_val)
-    z_curl_interpolated = NearestNDInterpolator(z, z_val)
+    x_interpolated = NearestNDInterpolator(x, x_val)
+    y_interpolated = NearestNDInterpolator(y, y_val)
+    z_interpolated = NearestNDInterpolator(z, z_val)
 
-    return x_curl_interpolated, y_curl_interpolated, z_curl_interpolated
+    return x_interpolated, y_interpolated, z_interpolated
 
 
+# Input of this function are the x-,y- and z-faces and the curl values of the electric field on the faces
+# Or x-,y- and z-edges and the values of the electric field
+# Linear interpolation
 def interpolate_linear(x, y, z, x_val, y_val, z_val):
-    x_curl_interpolated = LinearNDInterpolator(x, x_val)
-    y_curl_interpolated = LinearNDInterpolator(y, y_val)
-    z_curl_interpolated = LinearNDInterpolator(z, z_val)
+    x_interpolated = LinearNDInterpolator(x, x_val)
+    y_interpolated = LinearNDInterpolator(y, y_val)
+    z_interpolated = LinearNDInterpolator(z, z_val)
 
-    return x_curl_interpolated, y_curl_interpolated, z_curl_interpolated
+    return x_interpolated, y_interpolated, z_interpolated
 
 
 def estimate_curl_electric_field(mesh, survey, model_map, model, interpolation='rbf', frequency=1.0,
-                                 omega=2 * np.pi):
+                                 omega=2 * np.pi, parameter='resistivity'):
     x_faces = mesh.faces_x
     y_faces = mesh.faces_y
     z_faces = mesh.faces_z
@@ -48,13 +61,23 @@ def estimate_curl_electric_field(mesh, survey, model_map, model, interpolation='
 
     # Solution by forward modelling for magnetic flux density and electric field
     # This uses a rhoMap (resistivity)
-    simulation = fdem.simulation.Simulation3DMagneticFluxDensity(
-        mesh, survey=survey, rhoMap=model_map, Solver=Solver
-    )
+    if parameter == 'resistivity':
+        simulation = fdem.simulation.Simulation3DMagneticFluxDensity(
+            mesh, survey=survey, rhoMap=model_map, Solver=Solver
+        )
 
-    simulationelectricfield = fdem.simulation.Simulation3DElectricField(
-        mesh, survey=survey, rhoMap=model_map, Solver=Solver
-    )
+        simulationelectricfield = fdem.simulation.Simulation3DElectricField(
+            mesh, survey=survey, rhoMap=model_map, Solver=Solver
+        )
+    # This uses a sigmaMap (conductivity)
+    else:
+        simulation = fdem.simulation.Simulation3DMagneticFluxDensity(
+            mesh, survey=survey, sigmaMap=model_map, Solver=Solver
+        )
+
+        simulationelectricfield = fdem.simulation.Simulation3DElectricField(
+            mesh, survey=survey, sigmaMap=model_map, Solver=Solver
+        )
 
     # Compute magnetic flux density
     fields = simulation.fields(model)
@@ -98,7 +121,6 @@ def estimate_curl_electric_field(mesh, survey, model_map, model, interpolation='
 
 
 def compute_cell_error(cell, curl_x, curl_y, curl_z, ef_x, ef_y, ef_z):
-
     def ef_interpolator(x):
         return np.array([ef_x(*x), ef_y(*x), ef_z(*x)])
 
@@ -112,12 +134,10 @@ def compute_cell_error(cell, curl_x, curl_y, curl_z, ef_x, ef_y, ef_z):
     return error
 
 
-def estimate_error(mesh, survey, model_map, model, search_area, interpolation='rbf', frequency=1.0,
-                   omega=2 * np.pi, refine_percentage=0.05):
-    curl_x, curl_y, curl_z, ef_x, ef_y, ef_z = estimate_curl_electric_field(mesh, survey, model_map,
-                                                                            model, interpolation,
-                                                                            frequency, omega)
-
+def estimate_error(mesh, survey, model_map, model, search_area, curl_x, curl_y, curl_z
+                   , ef_x, ef_y, ef_z
+                   , interpolation='rbf', frequency=1.0,
+                   omega=2 * np.pi, refine_percentage=0.05, parameter='resistivity'):
     cell_errors = []
     for cell in search_area:
         error = compute_cell_error(cell, curl_x, curl_y, curl_z, ef_x, ef_y, ef_z)
@@ -127,3 +147,99 @@ def estimate_error(mesh, survey, model_map, model, search_area, interpolation='r
     n_refine_cells = int(np.ceil(refine_percentage * len(search_area)))
     cells_to_refine = search_area[np.argpartition(cell_errors, -n_refine_cells)[-n_refine_cells:]]
     return cells_to_refine
+
+
+def iterator(mesh, domain, surface, cell_width, objct, create_surface
+             , x_object, y_object, z_object
+             , receiver_locations, source_locations, survey, par_background, par_object,
+             model_map, model, ind_object, frequency=1, omega=2 * np.pi
+             , parameter='resistivity', interpolation='rbf', type_object='box'
+             , lim_iterations=5, factor_object=2, factor_receiver=3
+             , refine_percentage=0.05, axis='x', degrees_rad=0, radius=1):
+    diff = 10
+    i = 0
+
+    def ef_interpolator(x):
+        return np.array([ef_x(*x), ef_y(*x), ef_z(*x)])
+
+    def ef_old_interpolator(x):
+        return np.array([ef_old_x(*x), ef_old_y(*x), ef_old_z(*x)])
+
+    # -------------------------------------------------------------------------------------------------
+    while diff > 0.01 and i < lim_iterations:
+        # -------------------------------------------------------------------------------------------------
+        # Define search areas
+        search_area_obj = search_area_object(mesh, objct, factor=factor_object)
+        search_area_receiv = search_area_receivers(mesh, receiver_locations,
+                                                         factor=factor_receiver)
+        # Interpolate curl and electric field
+        curl_x, curl_y, curl_z, ef_x, ef_y, ef_z = estimate_curl_electric_field(mesh, survey,
+                                                                                model_map,
+                                                                                model,
+                                                                                interpolation=interpolation,
+                                                                                frequency=frequency,
+                                                                                omega=omega
+                                                                                ,
+                                                                                parameter=parameter)
+        # Compare electric field values until relative difference falls below 1%
+        if i > 0:
+            relative_difference_Efield = []
+            for cell in search_area_obj:
+                # This equation is sensitive to catastrophic failure
+                form = np.abs(
+                    (ef_old_interpolator(cell) - ef_interpolator(cell)) / ef_old_interpolator(cell))
+                relative_difference_Efield.append(np.linalg.norm(form))
+            for cell in search_area_receiv:
+                # This equation is sensitive to catastrophic failure
+                form = np.abs(
+                    (ef_old_interpolator(cell) - ef_interpolator(cell)) / ef_old_interpolator(cell))
+                relative_difference_Efield.append(np.linalg.norm(form))
+            diff = max(relative_difference_Efield)
+            print("Maximum relative difference is ", diff)
+        ef_old_x = ef_x
+        ef_old_y = ef_y
+        ef_old_z = ef_z
+
+        # Define cells to refine near object
+        cells_to_refine_object = estimate_error(mesh, survey, model_map, model, search_area_obj
+                                                , curl_x, curl_y, curl_z
+                                                , ef_x, ef_y, ef_z
+                                                , interpolation=interpolation
+                                                , frequency=frequency, omega=omega
+                                                , refine_percentage=refine_percentage
+                                                , parameter=parameter)
+        # Define cells to refine near receivers
+        cells_to_refine_receivers = estimate_error(mesh, survey, model_map, model,
+                                                   search_area_receiv
+                                                   , curl_x, curl_y, curl_z
+                                                   , ef_x, ef_y, ef_z
+                                                   , interpolation=interpolation
+                                                   , frequency=frequency, omega=omega
+                                                   , refine_percentage=refine_percentage
+                                                   , parameter=parameter)
+        # Refine the mesh
+        mesh = create_octree_mesh(domain, cell_width, objct, 'surface')
+        if type_object == 'box':
+            surface_object = create_surface(x_object, y_object, z_object, axis, degrees_rad)
+        if type_object == 'sphere':
+            surface_object = create_surface(x_object, y_object, z_object, radius)
+        else:
+            pass
+        refine_at_locations(mesh, surface_object)
+        refine_at_locations(mesh, source_locations)
+        refine_at_locations(mesh, receiver_locations)
+        refine_at_locations(mesh, cells_to_refine_object)
+        refine_at_locations(mesh, cells_to_refine_receivers)
+        mesh.finalize()
+
+        # Find cells that are active in the forward modeling (cells below surface)
+        ind_active = surface2ind_topo(mesh, surface)
+
+        # Define mapping from model to active cells
+        model_map = maps.InjectActiveCells(mesh, ind_active, par_background)
+
+        # Define model. Models in SimPEG are vector arrays
+        model = par_background * np.ones(ind_active.sum())
+        model[ind_object(mesh, ind_active)] = par_object
+        i += 1
+    return mesh
--- src/Setup.py
+++ src/Setup.py
@@ -28,3 +28,47 @@ def define_survey(frequencies, receiver_locations, source_locations, num_transmi
     return survey
 
 
+def search_area_object(mesh, objct, factor=2):
+    # Factor determines the search range around the object
+    # Search area is always a rectangle, regardless of the shape of the object
+    cells = mesh.cell_centers
+    cell_width_X = min(mesh.h[0])  # minimum cell width in x-direction
+    cell_width_Y = min(mesh.h[1])  # minimum cell width in y-direction
+    cell_width_Z = min(mesh.h[2])  # minimum cell width in z-direction
+    Left_X = min(objct[:, 0])  # Left X
+    Right_X = max(objct[:, 0])  # Right X
+    Left_Y = min(objct[:, 1])  # Left Y
+    Right_Y = max(objct[:, 1])  # Right Y
+    Lower_Z = min(objct[:, 2])  # Lower Z
+    Upper_Z = max(objct[:, 2])  # Upper Z
+    search_area = cells[(cells[:, 0] > (Left_X - factor * cell_width_X)) & (
+                cells[:, 0] < (Right_X + factor * cell_width_X))
+                        & (cells[:, 1] > (Left_Y - factor * cell_width_Y)) & (
+                                    cells[:, 1] < (Right_Y + factor * cell_width_Y))
+                        & (cells[:, 2] > (Lower_Z - factor * cell_width_Z)) & (
+                                    cells[:, 2] < (Upper_Z + factor * cell_width_Z))]
+
+    return search_area
+
+
+def search_area_receivers(mesh, receiver_locations, factor=3):
+    # Factor determines the search range around the object
+    # Search area is always a rectangle, regardless of the shape of the object
+    cells = mesh.cell_centers
+    cell_width_X = min(mesh.h[0])  # minimum cell width in x-direction
+    cell_width_Y = min(mesh.h[1])  # minimum cell width in y-direction
+    cell_width_Z = min(mesh.h[2])  # minimum cell width in z-direction
+    Left_X = min(receiver_locations[:, 0])  # Left X
+    Right_X = max(receiver_locations[:, 0])  # Right X
+    Left_Y = min(receiver_locations[:, 1])  # Left Y
+    Right_Y = max(receiver_locations[:, 1])  # Right Y
+    Lower_Z = min(receiver_locations[:, 2])  # Lower Z
+    Upper_Z = max(receiver_locations[:, 2])  # Upper Z
+    search_area = cells[(cells[:, 0] > (Left_X - factor * cell_width_X)) & (
+                cells[:, 0] < (Right_X + factor * cell_width_X))
+                        & (cells[:, 1] > (Left_Y - factor * cell_width_Y)) & (
+                                    cells[:, 1] < (Right_Y + factor * cell_width_Y))
+                        & (cells[:, 2] > (Lower_Z - factor * cell_width_Z)) & (
+                                    cells[:, 2] < (Upper_Z + factor * cell_width_Z))]
+
+    return search_area
