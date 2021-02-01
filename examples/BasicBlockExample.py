7856ae2381b64786d11549cb7402824857048461
Tuhin Das <t.das@student.tudelft.nl>
2021-02-01 16:51:18 +0100

    Removed and cleaned the code.

--- examples/BasicBlockExample.py
+++ examples/BasicBlockExample.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-
 import warnings
-
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from discretize.utils import mkvc
@@ -11,65 +9,45 @@ import src.Meshing as M
 import src.Utils as utils
 from SimPEG.utils import surface2ind_topo
 from SimPEG import maps
-from src.LapentaEstimator import iterator
-import matplotlib.pyplot as plt
+from src.ErrorEstimator import iterator
 
+# Domain
 domain = ((-500, 4500), (-2000, 2000), (-2500, 200))
 cell_width = 50
 
+# Define the top geophysical surface
 xx, yy = np.meshgrid(np.linspace(-500, 4500, 101), np.linspace(-2000, 2000, 101))
 zz = np.zeros(np.shape(xx))
 surface = np.c_[mkvc(xx), mkvc(yy), mkvc(zz)]
 
+# Define the model
 box_coordinates = ((200, 600), (-500, 500), (-1000, -800))
-axis = 'z'
-angle = 15
-box_surface = M.create_box_surface(box_coordinates, cell_width, axis, angle)
+box_surface = M.create_box_surface(box_coordinates, cell_width)
 mesh = M.create_octree_mesh(domain, cell_width, box_surface)
 
 # Defining transmitter location
 xtx, ytx, ztx = np.meshgrid([0], [0], [0])
 source_locations = np.c_[mkvc(xtx), mkvc(ytx), mkvc(ztx)]
 ntx = np.size(xtx)
-print("Number of transmitters", len(source_locations))
-# wider area around transmitter
-# xa, ya, za = np.meshgrid(np.linspace(-200,200,101), np.linspace(-200,200,101), np.linspace(-200,200,101))
-# wider_area = np.c_[mkvc(xa), mkvc(ya), mkvc(za)]
 
 # Define receiver locations
-N = 21
-xrx, yrx, zrx = np.meshgrid(np.linspace(2000, 4000, N), [0], [0])
+num_receivers = 21
+xrx, yrx, zrx = np.meshgrid(np.linspace(2000, 4000, num_receivers), [0], [0])
 receiver_locations = np.c_[mkvc(xrx), mkvc(yrx), mkvc(zrx)]
+
+# Define survey
 frequencies = [1.0]  # Frequency (Hz)
 omegas = [2.0 * np.pi]  # Radial frequency (Hz)
-print("Number of receivers", len(receiver_locations))
-
 survey = utils.define_survey(frequencies, receiver_locations, source_locations, ntx)
 
 # Refine at certain locations
 M.refine_at_locations(mesh, source_locations)
-# M.refine_at_locations(mesh,wider_area)
 M.refine_at_locations(mesh, receiver_locations)
-
 mesh.finalize()
-
-for i in range(0, 65):
-    M.plot_mesh_slice(mesh, 'z', i, save=True)
-exit()
-
-# print(mesh)
-
-print("Total number of cells", mesh.nC)
-print("Total number of cell faces", mesh.n_faces)
-print("Total number of cell edges", mesh.n_edges)
 
 # Resistivity in Ohm m
 res_background = 1.0
 res_block = 100.0
-
-# Conductivity in S/m
-conductivity_background = 1 / 100.0
-conductivity_block = 1 / 1.0
 
 # Find cells that are active in the forward modeling (cells below surface)
 ind_active = surface2ind_topo(mesh, surface)
@@ -79,40 +57,19 @@ model_map = maps.InjectActiveCells(mesh, ind_active, res_background)
 
 # Define model. Models in SimPEG are vector arrays
 model = res_background * np.ones(ind_active.sum())
-
-ind_block = utils.get_ind_block(mesh, ind_active, box_coordinates, axis=axis, angle=angle)
+ind_block = utils.get_ind_block(mesh, ind_active, box_coordinates)
 model[ind_block] = res_block
 
+# Run the adaptive meshing algorithm
 mesh, ex, ey, ez, diff_list = iterator(mesh, domain, surface, cell_width, box_surface,
                                        box_coordinates
                                        , receiver_locations, source_locations, survey
                                        , res_background, res_block, model_map
-                                       , model, ind_block, lim_iterations=15, interpolation='rbf',
-                                       axis=axis, degrees_rad=angle)
+                                       , model, ind_block, lim_iterations=15, interpolation='rbf')
 
+# Print summary of the final mesh
 print(mesh)
-plt.figure(1)
-plt.plot(diff_list[:, 0], diff_list[:, 1])
-plt.xlabel('Number of iterations')
-plt.ylabel('Average relative difference curr. and prev. iteration')
-plt.title('Convergence of a sphere in an adaptive grid')
-plt.show()
 
-print(diff_list[:, 0])
-print(diff_list[:, 1])
-
-np.savetxt('rotated_block_relative_diff.csv', diff_list, delimiter=',')
-
-for i in range(0, 4000):
-    M.plot_mesh_slice(mesh, 'z', i, save=True)
-
-'''
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(cells_to_refine[:, 0], cells_to_refine[:, 1], cells_to_refine[:, 2])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
-# refine cells
-'''
+# Plot a few z-slices of the mesh
+for i in range(0, 64):
+    M.plot_mesh_slice(mesh, 'z', i, save=False)
---
+++ examples/SphereExample.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from discretize.utils import mkvc
+import numpy as np
+import src.Meshing as M
+import src.Utils as utils
+from SimPEG.utils import surface2ind_topo
+from SimPEG import maps
+from src.ErrorEstimator import iterator
+
+# Domain
+domain = ((-500, 4500), (-2000, 2000), (-2500, 200))
+cell_width = 50
+
+# Define the top geophysical surface
+xx, yy = np.meshgrid(np.linspace(-500, 4500, 101), np.linspace(-2000, 2000, 101))
+zz = np.zeros(np.shape(xx))
+surface = np.c_[mkvc(xx), mkvc(yy), mkvc(zz)]
+
+# Define the model
+sphere_origin = ((1000), (300), (-1200))
+radius = 600
+sphere_surface = M.create_sphere_surface(sphere_origin, radius, 101)
+mesh = M.create_octree_mesh(domain, cell_width, sphere_surface)
+
+# Defining transmitter location
+xtx, ytx, ztx = np.meshgrid([0], [0], [0])
+source_locations = np.c_[mkvc(xtx), mkvc(ytx), mkvc(ztx)]
+ntx = np.size(xtx)
+
+# Define receiver locations
+N = 21
+xrx, yrx, zrx = np.meshgrid(np.linspace(2000, 4000, N), [0], [0])
+receiver_locations = np.c_[mkvc(xrx), mkvc(yrx), mkvc(zrx)]
+
+# Define survey
+frequencies = [1.0]  # Frequency (Hz)
+omegas = [2.0 * np.pi]  # Radial frequency (Hz)
+survey = utils.define_survey(frequencies, receiver_locations, source_locations, ntx)
+
+# Refine at certain locations
+M.refine_at_locations(mesh, source_locations)
+M.refine_at_locations(mesh, receiver_locations)
+mesh.finalize()
+
+# Resistivity in Ohm m
+res_background = 1.0
+res_block = 100.0
+
+# Find cells that are active in the forward modeling (cells below surface)
+ind_active = surface2ind_topo(mesh, surface)
+
+# Define mapping from model to active cells
+model_map = maps.InjectActiveCells(mesh, ind_active, res_background)
+
+# Define model. Models in SimPEG are vector arrays
+model = res_background * np.ones(ind_active.sum())
+ind_sphere = utils.get_ind_sphere(mesh, ind_active, sphere_origin, radius)
+model[ind_sphere] = res_block
+
+# Run the adaptive meshing algorithm
+mesh, ex, ey, ez, diff_list = iterator(mesh, domain, surface, cell_width, sphere_surface,
+                                       sphere_origin
+                                       , receiver_locations, source_locations, survey
+                                       , res_background, res_block, model_map
+                                       , model, ind_sphere, lim_iterations=20, radius=radius,
+                                       interpolation='rbf', type_object='sphere')
+
+# Print summary of the final mesh
+print(mesh)
+
+# Plot a few z-slices of the mesh
+for i in range(0, 64):
+    M.plot_mesh_slice(mesh, 'z', i, save=False)
